b8d2f371d0b55bcf282d15f615d5841b626f596b
SKATHEKING <46651755+SKATHEKING@users.noreply.github.com>
2022-02-04 13:42:51 +0000

    Create reverse_string.java

---
+++ reverse_string.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Program
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine();
+		char[] arr = text.toCharArray();
+		
+		//your code goes here
+	
+	String reverted ="";
+	for (int i=arr.length-1;i>=0;i--) {
+    reverted += arr[i];
+	}
+	System.out.println(reverted);
+	}
+}
